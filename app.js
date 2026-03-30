e4e5b6157e511cfb09873142a30b29ccbda9c6e5
Emerelauwaonu Caleb <emerelauwaonucaleb@gmail.com>
2026-03-30 11:17:48 +0100

    update

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ connectDB();
 
 app.use (express.json());
 app.use (cors());
+app.use (LogIn);
+
+
+app.use (errorHandler);
 
 const PORT = process.env.PORT || 3000;
 app.listen (PORT, () => console.log('App is listening {PORT}'));
